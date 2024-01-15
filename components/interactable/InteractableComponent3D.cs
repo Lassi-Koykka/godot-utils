45b5b844c92e1ce8ded81cdf87a7f63c081a1caf
Lassi Köykkä <lassi_koykka@hotmail.com>
2024-01-15 20:30:05 +0200

    Format InteractableComponent3D

--- components/interactable/InteractableComponent3D.cs
+++ components/interactable/InteractableComponent3D.cs
@@ -1,40 +1,46 @@
 using Godot;
 
-public enum InteractionStatus {
-  Unavailable,
-  Available,
-  Failed,
-  InProgress,
-  Success,
+public enum InteractionStatus
+{
+    Unavailable,
+    Available,
+    Failed,
+    InProgress,
+    Success,
 }
 
-public partial class InteractableComponent3D : Area3D {
-  [Signal]
-  public delegate void InteractedEventHandler();
-  [Export]
-  public bool Enabled { get; set; } = true;
-  [Export] 
-  public float InteractionDuration { get; private set; } = 0;
-  public float InteractionProgress { get; private set; } = 0;
-  public bool InteractionActive { get; private set; } = false;
+public partial class InteractableComponent3D : Area3D
+{
+    [Signal]
+    public delegate void InteractedEventHandler();
+    [Export]
+    public bool Enabled { get; set; } = true;
+    [Export]
+    public float InteractionDuration { get; private set; } = 0;
+    public float InteractionProgress { get; private set; } = 0;
+    public bool InteractionActive { get; private set; } = false;
+    public string promptText { get; private set; } = "";
 
-  public override void _PhysicsProcess(double delta) {
-    var newProgress = InteractionProgress + (float)(InteractionActive ? delta : -delta);
-    InteractionProgress = Mathf.Clamp(newProgress, 0, InteractionDuration);
+    public override void _PhysicsProcess(double delta)
+    {
+        var newProgress = InteractionProgress + (float)(InteractionActive ? delta : -delta);
+        InteractionProgress = Mathf.Clamp(newProgress, 0, InteractionDuration);
 
-    InteractionActive = false;
-  }
-
-  public virtual (InteractionStatus, float) HandleInteract() {
-    InteractionActive = true;
-    var progress = (InteractionProgress / InteractionDuration);
-    if (InteractionDuration > 0 && InteractionProgress < InteractionDuration) {
-      return (InteractionStatus.InProgress, progress);
+        InteractionActive = false;
     }
 
-    EmitSignal(SignalName.Interacted);
-    Enabled = false;
-    InteractionActive = false;
-    return (InteractionStatus.Success, 1);
-  }
+    public virtual (InteractionStatus, float) HandleInteract()
+    {
+        InteractionActive = true;
+        var progress = (InteractionProgress / InteractionDuration);
+        if (InteractionDuration > 0 && InteractionProgress < InteractionDuration)
+        {
+            return (InteractionStatus.InProgress, progress);
+        }
+
+        EmitSignal(SignalName.Interacted);
+        Enabled = false;
+        InteractionActive = false;
+        return (InteractionStatus.Success, 1);
+    }
 }
